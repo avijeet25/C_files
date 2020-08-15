43b8acc604a4be5ca280353cdc635defbeb70e11
Avijeet <54774303+avijeet25@users.noreply.github.com>
2020-08-15 19:30:34 +0530

    forloop

--- testing_codes.c
+++ testing_codes.c
@@ -3,6 +3,9 @@ int main ()
     {
         int a,b,c;
         a=b=c=40;
-        printf("%d",a);
+        //printf("%d",a);
+        for (int i=5;i;i--)
+        printf("%d\n",i);
         return 0;
+
     }
