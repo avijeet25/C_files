68803c260567119d6f40df82eb5389b79f7b05b9
Avijeet <54774303+avijeet25@users.noreply.github.com>
2020-08-17 23:07:18 +0530

    pointersDemo

---
+++ demo_pointers.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main() {
+    int i = 9;
+    int *j;
+    j = &i;
+    printf("Add i = %u \n",&i);
+    printf("Value j = %u \n",j);
+    printf("Add j = %u \n",&j);
+    printf("Value i = %d \n",i);
+    printf("Value= i = %d \n",*(&i));
+    printf("Value i = %d \n",*j);
+    return 0;
+}
+
+/*
+Here i is stored at 6422220
+j 
+
+
+
+*/
