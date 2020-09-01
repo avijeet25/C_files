655d9e02882312d8937dfd1614ac4818af315847
Avijeet <54774303+avijeet25@users.noreply.github.com>
2020-09-01 10:45:39 +0530

    hackerrankday1

---
+++ demo_str_arr.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <string.h>
+/*
+int main()
+char string1 []="this is a test";
+int count;
+count= sizeof(string1);
+int i =0;
+for (i=0;i<count;i++){
+    printf("%d\n",string1[i]);
+return 0;
+}
+*/
---
+++ traffic_light.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <graphics.h>
+int main()
+{
+
+
+		initwindow(800, 800);
+		
+		rectangle(250, 50, 350, 350);
+		
+		circle(300, 100, 50);
+		circle(300, 200, 50);
+		circle(300, 300, 50);
+		
+		//creating animation
+		for(int i =0; i < 10; i++){
+			
+			setfillstyle(1, RED);
+			floodfill(300, 100, WHITE);
+			outtextxy(280, 100, "STOP");
+			delay(1000);
+			
+			
+			
+			setfillstyle(1, BLACK);
+			floodfill(300, 100, WHITE);
+			setfillstyle(1, YELLOW);
+			floodfill(300, 200, WHITE);
+			outtextxy(280, 200, "HOLD");
+			delay(1000);
+			
+			
+			setfillstyle(1, BLACK);
+			floodfill(300, 200, WHITE);
+			setfillstyle(1, GREEN);
+			floodfill(300, 300, WHITE);
+			outtextxy(280, 300, "GO");
+			delay(1000);
+			
+			setfillstyle(1, BLACK);
+			floodfill(300, 300, WHITE);
+			
+			
+		}
+		
+		
+		
+		
+		closegraph();
+    return 0;
+		
+	
+}
